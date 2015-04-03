650dfe96cd5656fe8fbc340284f2e1f064598647
Jouni Mäkitalo <jouni.a.makitalo@gmail.com>
2015-04-03 17:38:35 +0300

    Defined sandwich classes in thingy.hh.

--- src/thingy.hh
+++ src/thingy.hh
@@ -1,6 +1,16 @@
 #ifndef THINGY_HH
 #define  THINGY_HH
 
+class CSandwich
+{
+public:
+	virtual void eat();
+};
 
+class CBoloneySandwich : public CSandwich
+{
+public:
+	void eat() override;
+};
 
 #endif
