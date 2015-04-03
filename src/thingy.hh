6b7a68b5e6fa7bede0463d5af74d504e38e28bb4
Jouni Mäkitalo <jouni.a.makitalo@gmail.com>
2015-04-03 17:41:36 +0300

    Added calories to sandwich class.

--- src/thingy.hh
+++ src/thingy.hh
@@ -3,13 +3,16 @@
 
 class CSandwich
 {
+	float calories;
 public:
+	CSandwich() : calories(0f) {}
 	virtual void eat();
 };
 
 class CBoloneySandwich : public CSandwich
 {
 public:
+	CBoloneySandwitch() : calories(200f) {}
 	void eat() override;
 };
 
